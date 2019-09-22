5ef8ebf8844d92cb878fe6dfd66eec93e2c1f8c7
dingyajun <dingyajunzl@163.com>
2019-09-22 20:24:41 +0800

    test commit

---
+++ springboot-getui/src/main/java/com/dyj/getui/Test.java
@@ -0,0 +1,11 @@
+package com.dyj.getui;
+
+/**
+ * @author dyj
+ * @date 2019/9/22 20:23
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("first test");
+    }
+}
